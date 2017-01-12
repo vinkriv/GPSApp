18c13d3e0b85a53cdafa91bee3392863ff01a4c7
Vineeth Vattikuti <vinkriv@gmail.com>
2017-01-12 11:52:13 -0600

    new

--- app/src/main/java/com/example/a10017404/gpsapp/MainActivity.java
+++ app/src/main/java/com/example/a10017404/gpsapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,10 +29,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     LocationManager locationManager;
     TextView lat;
     TextView lon;
+    TextView loc;
     boolean perm;
     Button b1;
     Geocoder geocoder;
     List<Address> addresses;
+    Location oldlocation;
+    int totaldist;
+    TextView dist;
+    long initialtime;
+    long currenttime;
+    TextView time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +47,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
         lat = (TextView) findViewById(R.id.lat);
         lon = (TextView) findViewById(R.id.lon);
+        loc = (TextView)findViewById(R.id.loc);
         b1= (Button)findViewById(R.id.button);
+        time = (TextView)findViewById(R.id.time);
         geocoder = new Geocoder(this,Locale.US);
+        dist = (TextView)findViewById(R.id.dist);
+        initialtime = System.currentTimeMillis();
+        addresses = new ArrayList<>();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
             Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
@@ -47,17 +61,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
         }
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, MainActivity.this);
-                }catch (SecurityException e){
-
-                }
-            }
-        });
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, MainActivity.this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, MainActivity.this);
     }
 
     @Override
@@ -67,13 +71,22 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
+        if (oldlocation!=null&& SystemClock.elapsedRealtime()>=5000){
+            float distance = location.distanceTo(oldlocation);
+            totaldist+=distance;
+            dist.setText(String.valueOf(totaldist));
+        }
         lat.setText(String.valueOf(location.getLatitude()));
         lon.setText(String.valueOf(location.getLongitude()));
         try {
             addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            loc.setText(addresses.get(0).getAddressLine(0));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        oldlocation=location;
+        currenttime = System.currentTimeMillis();
+        time.setText("Text:" +((currenttime-initialtime)/1000));
     }
 
     @Override
